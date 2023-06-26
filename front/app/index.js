02577bda8b9dd43e68daab8e2b83d7acc8816178
Mathis <mathislag@hotmail.fr>
2023-06-26 17:01:02 +0200

    Ajout scan plaques

--- front/app/index.js
+++ front/app/index.js
@@ -1,5 +1,6 @@
 import {StyleSheet, View} from 'react-native';
 import Navbar from "../components/navbar";
+import CameraView from "../components/camPage";
 import {useSegments} from "expo-router";
 import React from "react";
 
@@ -7,6 +8,7 @@ export default function Page() {
     const segments = useSegments()[0];
   return (
       <View style={styles.container}>
+          <CameraView></CameraView>
           <Navbar route={segments}></Navbar>
       </View>
   );
