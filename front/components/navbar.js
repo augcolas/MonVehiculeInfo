fb2e7fd3373b33e01dad0705bf6abb6b94e30a04
augcolas <augus.colas@orange.fr>
2023-06-27 08:52:00 +0200

    uploading api on http://minikit.pythonanywhere.com

--- front/components/navbar.js
+++ front/components/navbar.js
@@ -9,7 +9,7 @@ export default function Navbar({route}) {
     return (
         <View style={styles.container}>
             <View style={route === "sdsd" ? styles.item : [styles.item, styles.current]}><Link href="/profile"><Ionicons name="person-circle-outline" size={24} color="black" /></Link></View>
-            <View style={[styles.item]}><Link href="/vehicules"><Ionicons name="car-outline" size={24} color="black" /></Link></View>
+            <View style={[styles.item]}><Link href="/vehicles"><Ionicons name="car-outline" size={24} color="black" /></Link></View>
             <View style={[styles.item]}><Link href="/"><Ionicons name="md-camera-outline" size={24} color="black" /></Link></View>
             <View style={[styles.item]}><Link href="/chat"><Ionicons name="chatbox-outline" size={24} color="black" /></Link></View>
             <View style={[styles.item]}><Link href="/settings"><Ionicons name="ios-settings-outline" size={24} color="black" /></Link></View>
