1db3b53ba8b6613976de847516f0aa6beaa28f10
Candau Sebastien <sebastien.candau@etu.u-bordeaux.fr>
2023-06-28 09:42:19 +0200

    user on storage

--- front/mainApp.js
+++ front/mainApp.js
@@ -1,13 +1,25 @@
+import { useEffect } from "react";
 import {useAuth} from "./context/Auth";
 import Tabs from "./components/Tabs";
 import * as React from "react";
 import LoginScreen from "./screens/Auth/LoginScreen";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function MainApp() {
-    const {isLoggedIn} = useAuth();
+    const {isLoggedIn, logIn} = useAuth();
 
     const Stack = createNativeStackNavigator();
+
+    useEffect(() => {
+        const verifyUser = async () => {
+            const user = await AsyncStorage.getItem('user');
+            if(user){
+                logIn(user);
+            }
+        }
+        verifyUser();
+    }, []);
 
 
     return (
