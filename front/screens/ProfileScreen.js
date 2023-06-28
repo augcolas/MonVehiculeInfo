5867e6c6ddf893883011400f2664f451aa554633
augcolas <augus.colas@orange.fr>
2023-06-28 11:08:35 +0200

    Updating Profile SCREEN

--- front/screens/ProfileScreen.js
+++ front/screens/ProfileScreen.js
@@ -1,9 +1,23 @@
-import {StyleSheet, Text, View} from "react-native";
+import {StyleSheet, Text, View, ScrollView} from "react-native";
+import React, {useEffect} from "react";
+import {useAuth} from "../context/Auth";
 
 export default function ProfileScreen() {
+    const {authUser} = useAuth();
+
+    useEffect(() => {
+        console.log('authUser',authUser)
+    },[])
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>PROFILE SCREEN</Text>
+            <ScrollView contentContainerStyle={styles.scrollViewContainer}>
+                <Text style={styles.title}>Votre Profil</Text>
+
+                <View style={styles.pinfos}>
+                    <Text>Prénom : {authUser.name}</Text>
+                    <Text>Mail : </Text>
+                </View>
+            </ScrollView>
         </View>
     );
 }
@@ -11,11 +25,25 @@ export default function ProfileScreen() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#000',
+        backgroundColor: '#ffffff',
         alignItems: 'center',
         justifyContent: 'center'
     },
     text: {
         color: "#fff"
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 8,
+    },
+    scrollViewContainer: {
+        alignItems: 'center',
+        flexGrow: 1,
+        paddingTop: 50,
+        paddingBottom: 100,
+    },
+    pinfos: {
+        marginTop: 50,
     }
 });
