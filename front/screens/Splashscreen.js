0464ea83390489eedaa456296490073bb3d954ee
fchabellard <felix.chabellard@etu.u-bordeaux.fr>
2023-07-04 10:34:34 +0200

    test

--- front/screens/Splashscreen.js
+++ front/screens/Splashscreen.js
@@ -1,22 +1,21 @@
 import { useCallback, useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
-import {useNavigation} from "@react-navigation/native";
+import { useNavigation } from "@react-navigation/native";
 import Entypo from '@expo/vector-icons/Entypo';
 import * as SplashScreen from 'expo-splash-screen';
 import * as Font from 'expo-font';
-import { RotateInDownLeft, ZoomInRotate } from 'react-native-reanimated';
 
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-
+  
   const [appIsReady, setAppIsReady] = useState(false);
   const navigation = useNavigation();
 
   useEffect(() => {
     async function prepare() {
       try {
-        await Font.loadAsync(Entypo.font);
+        //await Font.loadAsync(Entypo.font);
         await new Promise(resolve => setTimeout(resolve, 2000));
       } catch (e) {
         console.warn(e);
@@ -35,21 +34,21 @@ export default function App() {
 
   if (!appIsReady) {
     return (
-      <View style={styles.splash} onLayout={onLayoutRootView}>
-        <Entypo name="info-with-circle" size={60} style={styles.icon} />
+      <View style={styles.container} onLayout={onLayoutRootView}>
+        <Entypo style={styles.icon} name="info-with-circle" size={60} />
         <Text style={styles.title}>M.V.I</Text>
       </View> 
     );
   } else {
     return (
-      navigation.navigate('Login')
+      navigation.navigate('Login', { params })
     )
   }
 
 }
 
 const styles = {
-  splash: {
+  container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
